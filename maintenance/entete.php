0d179b63283b8e3c89e8b017a619382924aa2db4
Teyir <60007549+Teyir@users.noreply.github.com>
2021-03-13 12:36:52 +0100

    Ez ez

--- maintenance/entete.php
+++
@@ -1,3 +0,0 @@
-<div class="alert alert-danger" style="text-align: center;">
-  <span class="glyphicon glyphicon-warning-sign"></span> Le mode maintenance a été activé , seul les administrateurs on accès au site internet ! <span class="glyphicon glyphicon-warning-sign"></span>
-</div>
